9457a2b542b4082a37932a93d761b460a7c1b67c
Adrienne Domingus <adrienne.domingus@gmail.com>
2015-12-28 09:20:38 -0700

    refactor

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -3,16 +3,14 @@ class Hamming
   VERSION = 1
 
   def self.compute(strand1, strand2)
-    strand1 = strand1.chars
-    strand2 = strand2.chars
-    diff = 0
     if strand1.length != strand2.length
-      raise ArgumentError.new
-    else
-      strand1.each_with_index do |char, index|
-        diff += 1 if strand1[index] != strand2[index]
-      end
-      diff
+      raise ArgumentError.new("Looks like your strand lengths aren't the same.")
     end
+    strand1, strand2 = strand1.chars, strand2.chars
+    diff = 0
+    strand1.each_with_index do |char, index|
+      diff += 1 if strand1[index] != strand2[index]
+    end
+    diff
   end
 end
