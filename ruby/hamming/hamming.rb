625286a764baaaec91c6743a230448a9c6289814
Adrienne Domingus <adrienne.domingus@gmail.com>
2015-12-28 19:05:31 -0700

    separate error to own method

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -3,14 +3,18 @@ class Hamming
   VERSION = 1
 
   def self.compute(strand1, strand2)
-    if strand1.length != strand2.length
-      raise ArgumentError.new("Looks like your strand lengths aren't the same.")
-    end
+    verify_strands(strand1, strand2)
     strand1, strand2 = strand1.chars, strand2.chars
     diff = 0
     strand1.each_with_index do |char, index|
       diff += 1 if strand1[index] != strand2[index]
     end
     diff
+  end
+
+  def self.verify_strands(strand1, strand2)
+    if strand1.length != strand2.length
+      raise ArgumentError.new("Looks like your strand lengths aren't the same.")
+    end
   end
 end
