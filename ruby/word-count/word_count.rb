4705a8b57631036183bebc58204aa13442516aa1
Adrienne Domingus <adrienne.domingus@gmail.com>
2015-12-30 07:04:02 -0700

    word count exercism

---
+++ ruby/word-count/word_count.rb
@@ -0,0 +1,25 @@
+class Phrase
+
+  VERSION = 1
+
+  attr_reader :phrase
+
+  def initialize(phrase)
+    @phrase = phrase.downcase.gsub(/[^a-z0-9\s']/i, ' ')
+  end
+
+  def word_count
+    word_frequencies = Hash.new(0)
+    phrase.split.each do |word|
+      remove_quotations(word)
+      word_frequencies[word] +=1
+    end
+    word_frequencies
+  end
+
+  def remove_quotations(word)
+    if word.start_with?("'")
+      word.gsub!("'", "")
+    end
+  end
+end
