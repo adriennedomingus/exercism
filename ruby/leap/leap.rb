c3a6ac16335bdd7d33f1125f602226b4551250dd
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-01-03 09:19:34 -0700

    refactor

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -3,14 +3,19 @@ class Year
   VERSION = 1
 
   def self.leap?(year)
-    every_four_not_100(year) || every_400(year)
+    divisible_by_400(year) || divisible_by_4(year) && !divisible_by_100(year)
   end
 
-  def self.every_four_not_100(year)
-    year % 4 == 0 && year % 100 != 0
+  def self.divisible_by_4(year)
+    year % 4 == 0
   end
 
-  def self.every_400(year)
+  def self.divisible_by_100(year)
+    year % 100 == 0
+
+  end
+
+  def self.divisible_by_400(year)
     year % 400 == 0
   end
 
