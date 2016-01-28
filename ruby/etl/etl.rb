8149ad9d9dd6d5f8d050e1dda86a3b64e6d9807d
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-01-28 06:36:22 -0700

    etl first iteration

---
+++ ruby/etl/etl.rb
@@ -0,0 +1,12 @@
+class ETL
+
+  def self.transform(old)
+    new = {}
+    old.each do |score, letters|
+      letters.each do |letter|
+        new[letter.downcase] = score
+      end
+    end
+    new
+  end
+end
