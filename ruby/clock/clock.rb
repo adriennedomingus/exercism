fcc69a09478e6a7f2b0c6fc70d18272add63705c
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-01-31 11:36:42 -0700

    clock exercism

---
+++ ruby/clock/clock.rb
@@ -0,0 +1,39 @@
+class Clock
+  attr_accessor :time
+
+  def initialize(hour, minutes = 0)
+    @time = hour * 60 + minutes
+  end
+
+  def self.at(hour, minutes = 0)
+    new(hour, minutes)
+  end
+
+  def to_s
+    the_hour = time / 60
+    the_minutes = time % 60
+    if the_hour > 23
+      the_hour -= 24
+      "#{"%02d" % the_hour}:#{"%02d" % the_minutes}"
+    elsif the_hour < 0
+      the_hour += 24
+      "#{"%02d" % the_hour}:#{"%02d" % the_minutes}"
+    else
+      "#{"%02d" % the_hour}:#{"%02d" % the_minutes}"
+    end
+  end
+
+  def +(minutes_to_add)
+    self.time += minutes_to_add
+    self
+  end
+
+  def -(minutes_to_subtract)
+    self.time -= minutes_to_subtract
+    self
+  end
+
+  def ==(clock2)
+    to_s == clock2.to_s
+  end
+end
