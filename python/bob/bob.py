3be192716af3002c93a1263eb2e65448ac487ae6
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-05-11 13:28:17 -0600

    bob python iteration two

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -2,11 +2,11 @@ import re
 
 def hey(what):
     what = what.strip()
-    if what.upper() == what and what.lower() != what:
+    if what.isupper() and what.lower() != what:
         return 'Whoa, chill out!'
     elif what.endswith('?'):
         return 'Sure.'
-    elif len(re.sub(r'\W', '', what)) == 0:
+    elif len(what) == 0:
         return 'Fine. Be that way!'
     else:
         return 'Whatever.'
