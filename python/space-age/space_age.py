1f433a5d3753b55ff16c7e24111fabe13ad15290
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-05-16 09:32:30 -0600

    space age iteration two)

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,31 +1,27 @@
+YEARS_PER_EARTH_YEAR = {
+                            "mercury": 0.2408467,
+                            "venus"  : 0.61519726,
+                            "mars"   : 1.8808158,
+                            "jupiter": 11.862615,
+                            "saturn" : 29.447498,
+                            "uranus" : 84.016846,
+                            "neptune": 164.79132
+                        }
+
+SECONDS_PER_EARTH_YEAR = 31557600.0
+
 class SpaceAge(object):
 
     def __init__(self, seconds):
         self.seconds = seconds
 
+    def __getattr__(self, planet):
+        def _missing():
+            return self.calculate(YEARS_PER_EARTH_YEAR[planet.split('_')[1]])
+        return _missing
+
     def on_earth(self):
-        return round(self.seconds / 31557600.0, 2)
-
-    def on_mercury(self):
-        return self.calculate(0.2408467)
-
-    def on_venus(self):
-        return self.calculate(0.61519726)
-
-    def on_mars(self):
-        return self.calculate(1.8808158)
-
-    def on_jupiter(self):
-        return self.calculate(11.862615)
-
-    def on_saturn(self):
-        return self.calculate(29.447498)
-
-    def on_uranus(self):
-        return self.calculate(84.016846)
-
-    def on_neptune(self):
-        return self.calculate(164.79132)
+        return round(self.seconds / SECONDS_PER_EARTH_YEAR, 2)
 
     def calculate(self, years_per_earth):
         return round(self.on_earth() * ( 1 / years_per_earth), 2)
