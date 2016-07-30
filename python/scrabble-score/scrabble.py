f78853402bcc6d848a9f1f6a080dfecd2e07efe9
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-07-30 10:04:45 -0700

    python scrabble score

---
+++ python/scrabble-score/scrabble.py
@@ -0,0 +1,39 @@
+import re
+
+POINT_MAAPING = {
+    'A': 1,
+    'B': 3,
+    'C': 3,
+    'D': 2,
+    'E': 1,
+    'F': 4,
+    'G': 2,
+    'H': 4,
+    'I': 1,
+    'J': 8,
+    'K': 5,
+    'L': 1,
+    'M': 3,
+    'N': 1,
+    'O': 1,
+    'P': 3,
+    'Q': 10,
+    'R': 1,
+    'S': 1,
+    'T': 1,
+    'U': 1,
+    'V': 4,
+    'W': 4,
+    'X': 8,
+    'Y': 4,
+    'Z': 10,
+}
+
+def score(word):
+    score = 0
+    for letter in word:
+        if letter.upper() not in POINT_MAAPING.keys():
+            return 0
+    for letter in word:
+        score += POINT_MAAPING[letter.upper()]
+    return score
